d2172969a54250dc7b79b6e99bbc4a847cee19d7
mrhatman26 <{ID}+{username}@users.noreply.github.com>
2024-02-25 11:20:13 +0000

    Added Plant class

--- core/src/com/mygdx/autofarm/AutoFarm.java
+++ core/src/com/mygdx/autofarm/AutoFarm.java
@@ -1,31 +1,37 @@
 package com.mygdx.autofarm;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class AutoFarm extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+	OrthographicCamera camera;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("badlogic.jpg"); //Todo: Replace badlogic.jpg with the background.
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
 		batch.end();
+		camera.update();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
 	}
 }
---
+++ core/src/com/mygdx/autofarm/Plant.java
@@ -0,0 +1,23 @@
+package com.mygdx.autofarm;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Plant {
+    private int harvestingTimer, wateringTimer, feedingTimer, dyingTimer;
+    private boolean grown, failed;
+    private Texture sprite;
+    public Plant(int harvestingTimer, int wateringTimer, int feedingTimer, int dyingTimer){
+        this.harvestingTimer = harvestingTimer;
+        this.wateringTimer = wateringTimer;
+        this.feedingTimer = feedingTimer;
+        this.dyingTimer = dyingTimer;
+        this.grown = false;
+        this.failed = false;
+        this.sprite = new Texture(Gdx.files.internal("badlogic.jpg")); //Todo: Replace badlogic.jpg with the background.
+    }
+
+    public void dispose(){
+        sprite.dispose();
+    }
+}
