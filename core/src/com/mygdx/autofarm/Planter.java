5f53cb3a60e696bc1d60cdb9f410a0a8341e1676
mrhatman26 <{ID}+{username}@users.noreply.github.com>
2024-02-25 11:30:59 +0000

    Added Planter class

---
+++ core/src/com/mygdx/autofarm/Planter.java
@@ -0,0 +1,21 @@
+package com.mygdx.autofarm;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
+
+public class Planter {
+    private int[] position, targetPosition;
+    private boolean override;
+    private Texture sprite;
+    public Planter(int[] position){
+        this.position = position;
+        this.targetPosition = new int[2];
+        this.sprite = new Texture(Gdx.files.internal("badlogic.jpg")); //Todo: Replace badlogic.jpg with the background.
+
+    }
+
+    public void dispose(){
+        sprite.dispose();
+    }
+}
