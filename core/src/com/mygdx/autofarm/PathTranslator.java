87b9737bbb2cc65de002e80f27b743a7a2a8bafd
mrhatman26 <{ID}+{username}@users.noreply.github.com>
2024-03-08 14:41:49 +0000

    Added Code to readPaths Method

--- core/src/com/mygdx/autofarm/PathTranslator.java
+++ core/src/com/mygdx/autofarm/PathTranslator.java
@@ -1,10 +1,35 @@
 package com.mygdx.autofarm;
 
+import com.badlogic.gdx.Gdx;
+
+import java.io.File;
+import java.util.Scanner
+
 public class PathTranslator {
-    private static String[] pathDescList;
+    private static String pathDescList;
+    private static File pathsFile;
+    private static Scanner pathsFileScanner;
 
     public static boolean readPaths(){
-        //ToDo
+        try{
+            pathsFile = Gdx.files.internal("paths.txt").file();
+            pathsFileScanner = new Scanner(pathsFile);
+            pathDescList = "";
+            while (pathsFileScanner.hasNextLine()){
+                if (pathDescList.equals("")){
+                    pathDescList = pathsFileScanner.nextLine();
+                }
+                else{
+                    pathDescList = pathDescList + "+" + pathsFileScanner.nextLine();
+                }
+            }
+            return true;
+        }
+        catch (Exception error){
+            System.out.println("(PathTranslator.java): An error occurred when trying to read paths.txt");
+            error.printStackTrace();
+            return false;
+        }
     }
 
     public static boolean translatePaths(){
